e842995dafb7d7c31317ffa5da3a34dd3bf5772e
Vinicius Vicentini <viniciusvicentini@live.com>
2026-04-08 19:07:23 -0600

    updating pill example

--- registry/react/examples/badge/example-pill.tsx
+++ registry/react/examples/badge/example-pill.tsx
@@ -1,34 +1,5 @@
-import { PlusIcon } from "lucide-react";
 import { Badge } from "@/registry/react/components/badge";
 
-const Example = () => (
-  <div className="flex flex-col gap-2">
-    <div className="flex flex-wrap gap-2">
-      <Badge pill size="sm">
-        Badge
-      </Badge>
-      <Badge pill size="md">
-        Badge
-      </Badge>
-      <Badge pill size="lg">
-        Badge
-      </Badge>
-    </div>
-    <div className="flex flex-wrap gap-2">
-      <Badge pill size="sm">
-        <PlusIcon />
-        Badge
-      </Badge>
-      <Badge pill size="md">
-        <PlusIcon />
-        Badge
-      </Badge>
-      <Badge pill size="lg">
-        <PlusIcon />
-        Badge
-      </Badge>
-    </div>
-  </div>
-);
+const Example = () => <Badge pill>Badge</Badge>;
 
 export default Example;
